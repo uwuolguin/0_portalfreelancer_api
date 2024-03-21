5f8fee33e9e3686fd8e168f961448765b98696f2
Andrés Olguín Dev <106123788+uwuolguin@users.noreply.github.com>
2024-03-21 12:18:44 -0300

    a

--- templates/0.DEV_scripts_udf.js
+++ templates/0.DEV_scripts_udf.js
@@ -123,3 +123,47 @@ async function deleteTalent() {
   }, "5000");
 
 } 
+
+async function postLogIn() {
+
+  document.getElementById("button_post_log_in").disabled = true;
+
+  btn=document.querySelector(".submission_button_login");
+  btn.classList.toggle("submission_button_login--loading");
+
+  oFormObject = document.forms['form6'];
+
+  email=oFormObject.elements["email"].value
+  password=oFormObject.elements["password"].value
+
+  
+
+
+
+  const body = new FormData
+  body.append("email", email)
+  body.append("", "\\")
+  body.append("password", password)
+
+
+  await fetch("https://apiportalfreelancer.lat/auth/login_talent_firm", {
+    body,
+    headers: {
+    Accept: "application/json"
+    },
+    method: "POST"
+  })
+  .then((response) => post_succesful_visible(response))
+  .catch((error) => post_failed_visible(error));
+
+
+  btn.classList.remove("submission_button_login--loading");
+
+  document.getElementById("button_post_log_in").disabled = false;
+
+  setTimeout(() => {
+    document.getElementById("succesful_post").classList.add("hidden_div");
+    document.getElementById("failed_post").classList.add("hidden_div");
+  }, "5000");
+
+} 
