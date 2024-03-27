fb81d08a43ad5cd46e80f6440232ee6f7ba20530
Andrés Olguín Dev <106123788+uwuolguin@users.noreply.github.com>
2024-03-27 12:47:36 -0300

    a

--- app/routers/contacts.py
+++ app/routers/contacts.py
@@ -333,11 +333,18 @@ def contacts_normal(request: Request,login: str = Cookie(None)):
     if not talents :
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= "BBDD does not have any record")
     
+
+    corrected_facebook=''
+    corrected_instagram=''
+    
     Talents_List=[]
 
     for  talent in talents:
         talent_dict={'id':str(talent.get("id")),'email':talent.get("email"),'full_name':talent.get("full_name"),'profession':talent.get("profession"),'rate':str(talent.get("rate")),'description':talent.get("description"),'github':talent.get("github"),'linkedin':talent.get("linkedin"),'instagram':talent.get("instagram"),'facebook':talent.get("facebook"),'skills':talent.get("skills"),'categories':talent.get("categories")}
         Talents_List.append(talent_dict)
+
+
+
 
     context={'request': request, 'categories':Categories_List,'skills':Skills_List,'talents':Talents_List}
     return templates.TemplateResponse("2_find_talent.html",context)
