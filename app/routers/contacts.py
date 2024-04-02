c9c7702c29d49c05ba3d7d04fb2483db7bdfcd72
Andrés Olguín Dev <106123788+uwuolguin@users.noreply.github.com>
2024-04-01 22:36:52 -0300

    a

--- app/routers/contacts.py
+++ app/routers/contacts.py
@@ -368,8 +368,6 @@ def contacts_normal(request: Request,login: str = Cookie(None)):
         if instagram_c =='None':
             instagram_c =''
 
-
-
         talent_dict={'id':str(talent.get("id")),'email':talent.get("email"),'full_name':talent.get("full_name"),'profession':talent.get("profession"),'rate':str(talent.get("rate")),'description':talent.get("description"),'github':talent.get("github"),'linkedin':talent.get("linkedin"),'instagram':instagram_c,'facebook':facebook_c,'skills':talent.get("skills"),'categories':talent.get("categories")}
 
         Talents_List.append(talent_dict)
