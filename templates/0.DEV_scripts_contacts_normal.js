8b52b4960126179bbf01da27c40c6c3b91a61f5c
Andrés Olguín Dev <106123788+uwuolguin@users.noreply.github.com>
2024-04-04 01:23:37 -0300

    a

--- templates/0.DEV_scripts_contacts_normal.js
+++ templates/0.DEV_scripts_contacts_normal.js
@@ -1,70 +1,67 @@
 
-function post_succesful_visible_2(response){
+function post_succesful_visible_2(response,id){
 
-
+  let id_text = id.toString();
+  suc_tex_id="succesful_post_"+id_text
   const status = ((response.status).toString()).substring(0, 1);
 
   if (status ==="2") {
-    document.getElementById("succesful_post").classList.remove("hidden_div")
-    document.getElementById("succesful_post").classList.remove("red_text")
-    document.getElementById("succesful_post").classList.add("green_text")
-    document.getElementById("succesful_post").innerHTML="Succesful Request"
+    document.getElementById(suc_tex_id).classList.remove("hidden_div")
+    document.getElementById(suc_tex_id).classList.remove("red_text")
+    document.getElementById(suc_tex_id).classList.add("green_text")
+    document.getElementById(suc_tex_id).innerHTML="Succesful Request"
 
   } else {
     
-    document.getElementById("succesful_post").classList.remove("hidden_div")
-    document.getElementById("succesful_post").classList.remove("green_text")
-    document.getElementById("succesful_post").classList.add("red_text")
-    document.getElementById("succesful_post").innerHTML="Failed Request. You can send and receive 1 email per day. We support the sending of 300 emails per day. Only Companies/Employers can contact Professionals"
+    document.getElementById(suc_tex_id).classList.remove("hidden_div")
+    document.getElementById(suc_tex_id).classList.remove("green_text")
+    document.getElementById(suc_tex_id).classList.add("red_text")
+    document.getElementById(suc_tex_id).innerHTML="Failed Request. You can send and receive 1 email per day. We support the sending of 300 emails per day. Only Companies/Employers can contact Professionals"
 
   }
 
 }
 
-function post_failed_visible(error){
+function post_failed_visible(error,id){
+
   console.log(error.detail)
-  document.getElementById("failed_post").classList.remove("hidden_div")
+  
+  let id_text_2 = id.toString();
+  fail_tex_id="failed_post_"+id_text_2
+  document.getElementById(fail_tex_id).classList.remove("hidden_div")
 
   
 }
 
-async function postLogIn() {
+async function postLogIn(id) {
 
-  document.getElementById("button_post_log_in").disabled = true;
+  let id_text_3 = id.toString();
+  button_tex_id="button_post_log_in_"+id_text_3
+  document.getElementById(button_tex_id).disabled = true;
 
   btn=document.querySelector(".submission_button_login");
   btn.classList.toggle("submission_button_login--loading");
 
-  oFormObject = document.forms['form7'];
-
-  email_sent=oFormObject.elements["email_sent"].value
+  url="https://apiportalfreelancer.lat/contacts/contacts/post/"+id_text_3
 
 
-  const body = new FormData
-  body.append("email_sent",  email_sent)
-
-
-
-  await fetch("https://apiportalfreelancer.lat/complaints/complaint_post/", {
-    body,
+   await fetch(url, {
     headers: {
-    Accept: "application/json"
+      Accept: "application/json"
     },
     method: "POST"
   })
-  .then((response) => post_succesful_visible_2(response))
-  .catch((error) => post_failed_visible(error));
+  .then((response) => post_succesful_visible_2(response,id=id))
+  .catch((error) => post_failed_visible(error,id=id));
 
 
   btn.classList.remove("submission_button_login--loading");
 
-  document.getElementById("button_post_log_in").disabled = false;
+  document.getElementById(button_tex_id).disabled = false;
 
   setTimeout(() => {
     document.getElementById("succesful_post").classList.add("hidden_div");
     document.getElementById("failed_post").classList.add("hidden_div");
   }, "5000");
   
-
-
 } 
