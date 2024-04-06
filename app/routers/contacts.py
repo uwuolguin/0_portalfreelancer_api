754cb671d2388b23c59d44bff4538844a64e9138
Andrés Olguín Dev <106123788+uwuolguin@users.noreply.github.com>
2024-04-06 19:04:41 -0300

    a

--- app/routers/contacts.py
+++ app/routers/contacts.py
@@ -293,7 +293,7 @@ def contacts_normal(  request: Request,
                       skills_state_string: Optional[str] = "None",
                       category_string: Optional[str] = "None",
                       category_state_string: Optional[str] = "None",
-                      magic_word:Optional[str] = "dancer", 
+                      magic_word:Optional[str] = "bounceR", 
                     #   magic_word:Optional[str] = "None", 
                       pagination_value:Optional[int] = 1, 
                       pagination_state:Optional[str] = "1.2.3.4.5.6.7.8.9.10"
