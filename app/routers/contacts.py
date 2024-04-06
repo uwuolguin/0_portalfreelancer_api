dd7672de8d5fce8c7de79fd174169dd5cc1dcb48
Andrés Olguín Dev <106123788+uwuolguin@users.noreply.github.com>
2024-04-06 18:05:50 -0300

    a

--- app/routers/contacts.py
+++ app/routers/contacts.py
@@ -352,6 +352,7 @@ def contacts_normal(  request: Request,
 
         query_part_1= "SELECT A.* FROM (SELECT id,email,full_name,profession,rate,description,github,linkedin,instagram,facebook,skills,categories,created_at FROM "+settings.table_name_for_select_all_free_user+" WHERE position("+magic_word_c+" in full_name)>0  ORDER by CREATED_AT LIMIT "+ str(limit_pagination)+") AS A ORDER BY CREATED_AT DESC LIMIT 3;"
 
+    print(query_part_1)
 
     cursor.execute(query_part_1)
     talents=cursor.fetchall()
