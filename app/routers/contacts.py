ee863302d8592ffc4757d9d92198859190f8eb32
Andrés Olguín Dev <106123788+uwuolguin@users.noreply.github.com>
2024-04-06 16:04:54 -0300

    a

--- app/routers/contacts.py
+++ app/routers/contacts.py
@@ -354,7 +354,10 @@ def contacts_normal(  request: Request,
     cursor.execute(query_part_1)
     talents=cursor.fetchall()
     if not talents :
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= "BBDD does not have any record")
+            query_part_1= "SELECT A.* FROM (SELECT id,email,full_name,profession,rate,description,github,linkedin,instagram,facebook,skills,categories,created_at FROM "+settings.table_name_for_select_all_free_user+" ORDER by CREATED_AT LIMIT "+ str(limit_pagination)+") AS A ORDER BY CREATED_AT DESC LIMIT 3;"
+
+            cursor.execute(query_part_1)
+            talents=cursor.fetchall()
     
     Talents_List=[]
 
