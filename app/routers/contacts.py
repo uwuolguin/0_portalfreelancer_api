d99d06dbec5b5f4b9ce507ef70481a29f6cb4e11
Andrés Olguín Dev <106123788+uwuolguin@users.noreply.github.com>
2024-04-06 16:37:02 -0300

    a

--- app/routers/contacts.py
+++ app/routers/contacts.py
@@ -348,8 +348,9 @@ def contacts_normal(  request: Request,
     else:
 
         magic_word_c ="'"+(magic_word.replace(" ", "")).lower()+"'"
+        magic_word_c2=" like '%' ||  LOWER(REPLACE("+magic_word+", ' ', '')) ||'%' "
 
-        query_part_1= "SELECT A.* FROM (SELECT id,email,full_name,profession,rate,description,github,linkedin,instagram,facebook,skills,categories,created_at FROM "+settings.table_name_for_select_all_free_user+" WHERE "+ magic_word_c +" like '%' ||  LOWER(REPLACE(full_name, ' ', '')) ||'%' OR "+ magic_word_c +" like '%' ||  LOWER(REPLACE(profession, ' ', '')) ||'%' OR "+ magic_word_c +" like '%' ||  LOWER(REPLACE(description, ' ', '')) ||'%' ORDER by CREATED_AT LIMIT "+ str(limit_pagination)+") AS A ORDER BY CREATED_AT DESC LIMIT 3;"
+        query_part_1= "SELECT A.* FROM (SELECT id,email,full_name,profession,rate,description,github,linkedin,instagram,facebook,skills,categories,created_at FROM "+settings.table_name_for_select_all_free_user+" WHERE full_name "+magic_word_c2+ " OR profession "+ magic_word_c2+ " OR description "+magic_word_c2+" ORDER by CREATED_AT LIMIT "+ str(limit_pagination)+") AS A ORDER BY CREATED_AT DESC LIMIT 3;"
 
 
     cursor.execute(query_part_1)
