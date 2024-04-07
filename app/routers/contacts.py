e0e1daeb63ada8c110dd799f276dc2128e1de232
Andrés Olguín Dev <106123788+uwuolguin@users.noreply.github.com>
2024-04-07 14:57:59 -0400

    A

--- app/routers/contacts.py
+++ app/routers/contacts.py
@@ -316,7 +316,7 @@ def contacts_normal(  request: Request,
                       category_state_string: Optional[str] = "DevelopmentandIT-category.AIServices-category",
                       pagination_state:Optional[str] = "1.2.3.4.5.6.7.8.9.10",
                       pagination_value:Optional[int] = 1, 
-                      magic_word:Optional[str] = "None"
+                      magic_word:Optional[str] = "BounCEr"
                       ):
 
     try:
