812cb21d76bd5b2367fc94f8310992c31bd878e0
Andrés Olguín Dev <106123788+uwuolguin@users.noreply.github.com>
2024-04-07 13:59:44 -0400

    a

--- app/routers/contacts.py
+++ app/routers/contacts.py
@@ -310,13 +310,13 @@ def PAGINATE_A_LIST(list_to_paginate,pagination_value_var):
 @router.get('/contacts_normal/',response_class=HTMLResponse)
 def contacts_normal(  request: Request,
                       login: str = Cookie(None),
-                      skills_string: Optional[str] = "PHP.Laravel",
+                      skills_string: Optional[str] = "None",
                       skills_state_string: Optional[str] = "None",
-                      category_string: Optional[str] = "Other",
+                      category_string: Optional[str] = "None",
                       category_state_string: Optional[str] = "None",
                       pagination_state:Optional[str] = "1.2.3.4.5.6.7.8.9.10",
                       pagination_value:Optional[int] = 1, 
-                      magic_word:Optional[str] = "dancER"
+                      magic_word:Optional[str] = "None"
                       ):
 
     try:
