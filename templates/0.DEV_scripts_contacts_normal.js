cfe8507ddc34cfc8b25f1d8805c545345c85f0fb
Andrés Olguín Dev <106123788+uwuolguin@users.noreply.github.com>
2024-04-06 22:24:53 -0300

    a

--- templates/0.DEV_scripts_contacts_normal.js
+++ templates/0.DEV_scripts_contacts_normal.js
@@ -214,6 +214,12 @@ if(glassValueStateButton==""){
 }
 
 
+
+skillStringValues = skillStringValues.slice(0, -1); 
+skillStringState = skillStringState.slice(0, -1); 
+categoryStringValues = categoryStringValues.slice(0, -1); 
+scategoryStringState = scategoryStringState.slice(0, -1); 
+
 console.log(skillStringValues)
 console.log(skillStringState)
 console.log(categoryStringValues)
