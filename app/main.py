5f2ef872c639f73b76c67841837beba59ff8b238
Andrés Olguín Dev <106123788+uwuolguin@users.noreply.github.com>
2024-04-07 23:18:21 -0400

    a

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from contextlib import asynccontextmanager
-from fastapi import FastAPI 
+from fastapi import FastAPI,status, HTTPException,Cookie,Request
 from .routers import contacts, firm, talent,complaints,donations,blacklistemail,blacklistwords,changepassword,auth,tableau,categories,skills
 from fastapi.middleware.cors import CORSMiddleware
 from .utils import conn_talent_utils
@@ -15,9 +15,12 @@ from .routers.categories import conn_categories
 from .routers.skills import conn_skills
 from fastapi.staticfiles import StaticFiles
 from .config import settings
-#PROBLEMAS DE CONTRASEÑA EL SERVIDOR LO ARREGLA Y POR GMAIL TE MANDA BOTON QUE TE CAMBIA LA PASSWORD Y LUEGO TE MANDA DENUEVO UN CORREO PERO CON LA PASSWORD QUE CREO
-#
-#En el front end, solo por si acaso, le voy a meter un html injection, aca por defecto ya se genera
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
+from typing import Optional
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -33,6 +36,7 @@ async def lifespan(app: FastAPI):
     conn_complaints.close()
     conn_categories.close()
     conn_skills.close()
+
 app= FastAPI(lifespan=lifespan)
 
 origins = ["https://apiportalfreelancer.lat/"
@@ -59,8 +63,59 @@ app.include_router(categories.router)
 app.include_router(skills.router)
 app.mount("/static", StaticFiles(directory=settings.picture_directory), name="static")
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+templates= Jinja2Templates(directory="./templates")
+
+@app.get('/',response_class=HTMLResponse)
+def root(  request: Request,
+                      login: str = Cookie(None),
+                      skills_string: Optional[str] = "None",
+                      skills_state_string: Optional[str] = "None",
+                      category_string: Optional[str] = "None",
+                      category_state_string: Optional[str] = "None",
+                      pagination_state:Optional[str] = "1.2.3.4.5.6.7.8.9.10",
+                      pagination_value:Optional[int] = 1, 
+                      magic_word:Optional[str] = "None"
+                      ):
+    
+
+    try:
+        conn.close()
+    except:
+        pass
+
+    while True:
+        try:
+
+            conn=psycopg2.connect(host=settings.database_hostname,database=settings.database_name,user=settings.database_username,password=settings.database_password,cursor_factory=RealDictCursor)
+            cursor=conn.cursor()
+            break
+
+        except Exception as error:
+
+            print("Connecting to database failed")
+            print("Error:",error)
+            time.sleep(5)
+    
+
+    cursor.execute(""" SELECT * FROM """+settings.table_name_for_select_all_categories+""" """)
+    categories=cursor.fetchall()
+    if not categories :
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= "BBDD does not have any record")
+    
+    
+    Categories_List=[]
+
+    for  category in categories:
+        category_dict={'category':category.get("category"),'category_key':category.get("category").replace(' ','')}
+        Categories_List.append(category_dict)
+    
+###################################### Talent Cache #######################
+        
+
+    if category_string !="None":
+        category_string_list=category_string.replace(' ','').lower().split('.')
 
 
+    context={'request': request, 'categories':Categories_List}
+    return templates.TemplateResponse("1_index.html",context)
