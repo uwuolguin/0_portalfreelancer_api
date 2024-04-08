4209d8bf74a8c8b90bc65a979287f350f0029945
Andrés Olguín Dev <106123788+uwuolguin@users.noreply.github.com>
2024-04-07 21:31:15 -0400

    a

--- templates/0.DEV_scripts_contacts_normal.js
+++ templates/0.DEV_scripts_contacts_normal.js
@@ -281,7 +281,7 @@ function lensButtonRequest(){
   // Pagiantion State General
    let paginationState= paginationState1+'.'+paginationState2+'.'+paginationState3+'.'+paginationState4+'.'+paginationState5+'.'+paginationState6+'.'+paginationState7+'.'+paginationState8+'.'+paginationState9+'.'+paginationState10
   
-  let paginationStateSelected= document.querySelector('.w3-button-active')
+  let paginationStateSelected= document.querySelector('.w3-button-m w3-button-active')
 
   paginationStateSelected= parseInt(paginationStateSelected)
 
