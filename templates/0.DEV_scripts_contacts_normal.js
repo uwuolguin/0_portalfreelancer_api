550fe0e4283e9058645d951162bd2bdbfad86541
Andrés Olguín Dev <106123788+uwuolguin@users.noreply.github.com>
2024-04-07 21:00:10 -0400

    a

--- templates/0.DEV_scripts_contacts_normal.js
+++ templates/0.DEV_scripts_contacts_normal.js
@@ -252,3 +252,136 @@ location.href =urlFinal
 }
 
 
+//SENDING REQUEST FOR HTML BY USING THE LENS BUTTON
+
+function lensButtonRequest(){
+
+  // DISABLE ALL BUTTONS UNTIL PAGE IS CHARGED
+  
+  document.querySelectorAll('.w3-button-m').forEach(elem => {
+    elem.disabled=true;
+    });
+  document.querySelectorAll('.submission_button_login').forEach(elem => {
+      elem.disabled=true;
+    });
+  document.querySelectorAll('.submission_button').forEach(elem => {
+      elem.disabled=true;
+    });
+  document.querySelectorAll('input[type="checkbox"]').forEach(elem => {
+      elem.disabled=true;
+    });
+    
+    paginationState1=document.getElementById("pagination_one").innerHTML
+    paginationState2=document.getElementById("pagination_two").innerHTML
+    paginationState3=document.getElementById("pagination_three").innerHTML
+    paginationState4=document.getElementById("pagination_four").innerHTML
+    paginationState5=document.getElementById("pagination_five").innerHTML
+    paginationState6=document.getElementById("pagination_six").innerHTML
+    paginationState7=document.getElementById("pagination_seven").innerHTML
+    paginationState8=document.getElementById("pagination_eight").innerHTML
+    paginationState9=document.getElementById("pagination_nine").innerHTML
+    paginationState10=document.getElementById("pagination_ten").innerHTML
+  
+  // Pagiantion State General
+   let paginationState= paginationState1+'.'+paginationState2+'.'+paginationState3+'.'+paginationState4+'.'+paginationState5+'.'+paginationState6+'.'+paginationState7+'.'+paginationState8+'.'+paginationState9+'.'+paginationState10
+  
+  let paginationStateSelected= document.querySelectorAll('.w3-button-active')
+  
+  let skillStringValues=''
+  let skillStringState=''
+  
+  let checkboxArraySkills=document.getElementById('checkbox_container_skills').getElementsByTagName('input')
+  
+  for (let i = 0; i < checkboxArraySkills.length; i++) {
+  
+        if (checkboxArraySkills[i].checked){
+        skillStringValues=skillStringValues+checkboxArraySkills[i].value+'.'
+        skillStringState=skillStringState+checkboxArraySkills[i].id+'.'
+  
+        }
+  
+  }
+  
+  if(skillStringValues==""){
+    
+    skillStringValues="None"
+  
+  } else {
+    skillStringValues = skillStringValues.slice(0, -1); 
+  }
+  
+  if(skillStringState==""){
+    
+    skillStringState="None"
+  
+  } else {
+  
+    skillStringState = skillStringState.slice(0, -1); 
+  
+  }
+  
+  let categoryStringValues=''
+  let categoryStringState=''
+  
+  let checkboxArrayCategories=document.getElementById('checkbox_container_categories').getElementsByTagName('input')
+  
+  for (let i = 0; i < checkboxArrayCategories.length; i++) {
+  
+        if (checkboxArrayCategories[i].checked){
+        categoryStringValues=categoryStringValues+checkboxArrayCategories[i].value+'.'
+        categoryStringState=categoryStringState+checkboxArrayCategories[i].id+'.'
+  
+        }
+  
+  }
+  
+  if(categoryStringValues==""){
+    
+    categoryStringValues="None"
+  
+  } else {
+    categoryStringValues = categoryStringValues.slice(0, -1); 
+  }
+  
+  if(categoryStringState==""){
+    
+    categoryStringState="None"
+  
+  } else {
+  
+  
+    categoryStringState = categoryStringState.slice(0, -1); 
+  }
+  
+  let glassValueStateButton= document.getElementById('lens_button').value
+  
+  if(glassValueStateButton==""){
+    
+    glassValueStateButton="None"
+  
+  }
+  console.log(paginationState)
+  console.log(paginationStateSelected)
+  console.log(skillStringValues)
+  console.log(skillStringState)
+  console.log(categoryStringValues)
+  console.log(categoryStringState)
+  console.log(glassValueStateButton)
+  
+  url_part1="https://apiportalfreelancer.lat/contacts/contacts_normal/?"
+  url_part2="skills_string="+skillStringValues+"&"
+  url_part3="skills_state_string="+skillStringState+"&"
+  url_part4="category_string="+categoryStringValues+"&"
+  url_part5="category_state_string="+categoryStringState+"&"
+  url_part6="pagination_state="+paginationState+"&"
+  url_part7="pagination_value="+paginationStateSelected+"&"
+  url_part8="magic_word="+glassValueStateButton
+  
+  urlFinal=url_part1+url_part2+url_part3+url_part4+url_part5+url_part6+url_part7+url_part8
+  
+  console.log(urlFinal)
+  
+  
+  location.href =urlFinal
+  
+  }
