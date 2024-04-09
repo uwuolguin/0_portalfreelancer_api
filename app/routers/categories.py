cfd02cbd7903fc0cc9ddb4e3b4668f3e23f65ff7
Andrés Olguín Dev <106123788+uwuolguin@users.noreply.github.com>
2024-04-09 00:03:46 -0400

    a

--- app/routers/categories.py
+++ app/routers/categories.py
@@ -15,33 +15,40 @@ router= APIRouter(
     tags=["Categories"]
 )
 
-while True:
-    try:
 
-        conn_categories=psycopg2.connect(host=settings.database_hostname,database=settings.database_name,user=settings.database_username,password=settings.database_password,cursor_factory=RealDictCursor)
-        cursor=conn_categories.cursor()
-        break
+def getConnection():
+    
 
-    except Exception as error:
+    while True:
+        try:
 
-        print("Connecting to database failed")
-        print("Error:",error)
-        time.sleep(5)
+            conn_categories=psycopg2.connect(host=settings.database_hostname,database=settings.database_name,user=settings.database_username,password=settings.database_password,cursor_factory=RealDictCursor)
+            break
+
+        except Exception as error:
+
+            print("Connecting to database failed")
+            print("Error:",error)
+            time.sleep(5)
+
+    return conn_categories
+
 
 @router.get("/categories_get_all",response_model=list[schemas.categoryResponse])
 def get_all_categories() -> Any:
 
     os.chdir(settings.normal_directory)
 
-    try:
-        conn_categories.rollback()
-    except:
-        pass
+    conn_categories=getConnection()
+    cursor=conn_categories.cursor()
+
 
     cursor.execute(""" SELECT * FROM """+settings.table_name_for_select_all_categories+""" """)
     category=cursor.fetchall()
     if not category :
+        conn_categories.close()
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= "BBDD does not have any record")
+    conn_categories.close()
     return category
 
 
@@ -54,17 +61,17 @@ def post_category(
 
     os.chdir(settings.normal_directory)
     
-    try:
-        conn_categories.rollback()
-    except:
-        pass
+    conn_categories=getConnection()
+    cursor=conn_categories.cursor()
 
     if login==None:
+        conn_categories.close()
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= "You need to be logged in to use this feature")
     
     credentials=oath2.decode_access_token(login)
 
     if dict(credentials).get("role") != "superadmin":
+        conn_categories.close()
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= "BAD CREDENTIALS")
    
 ###################CREATE USER#####################################################################
@@ -72,35 +79,38 @@ def post_category(
     category_2="""  SELECT insert_category('%s');"""
     cursor.execute(category_2 % (category_input))
     conn_categories.commit()
+    conn_categories.close()
 
 @router.delete("/category_delete/id/{category_input}",status_code=status.HTTP_204_NO_CONTENT)
 def delete_category(category_input:str, login: str = Cookie(None)):
 
     os.chdir(settings.normal_directory)
     
-    try:
-        conn_categories.rollback()
-    except:
-        pass
+    conn_categories=getConnection()
+    cursor=conn_categories.cursor()
 
     if login==None:
+        conn_categories.close()
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= "You need to be logged in to use this feature")
     
     credentials=oath2.decode_access_token(login)
 
     if dict(credentials).get("role") != "superadmin":
+        conn_categories.close()
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= "BAD CREDENTIALS")
     
     id_2=""" SELECT * FROM get_by_category('%s');"""
     cursor.execute(id_2 % (category_input))
     category=cursor.fetchone()
     if category ==None:
+        conn_categories.close()
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"BBDD with category: {category_input} does not exist")
 
     delete=""" SELECT * from delete_category('%s');"""
     cursor.execute(delete % (str(category_input)))
     conn_categories.commit()
 
+    conn_categories.close()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @router.put("/category_put/{category_input}/{new_category_input}")
@@ -108,17 +118,17 @@ def update_category(category_input:str,new_category_input:str, login: str = Cook
 
     os.chdir(settings.normal_directory)
     
-    try:
-        conn_categories.rollback()
-    except:
-        pass
+    conn_categories=getConnection()
+    cursor=conn_categories.cursor()
 
     if login==None:
+        conn_categories.close()
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= "You need to be logged in to use this feature")
     
     credentials=oath2.decode_access_token(login)
 
     if dict(credentials).get("role") != "superadmin":
+        conn_categories.close()
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= "BAD CREDENTIALS")
     
 
@@ -126,12 +136,14 @@ def update_category(category_input:str,new_category_input:str, login: str = Cook
     cursor.execute(id_2 % (category_input))
     category=cursor.fetchone()
     if category ==None:
+        conn_categories.close()
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"BBDD with category: {category_input} does not exist")
     
     update=""" SELECT * FROM update_category('%s','%s');"""
     cursor.execute(update % (category_input,new_category_input) )
     conn_categories.commit()
     
+    conn_categories.close()
     return Response(status_code=status.HTTP_201_CREATED,content='Data Updated') 
 
 
