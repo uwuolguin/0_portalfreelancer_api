d010b02536da13dfe51af6232d5ad318456de3d7
Andrés Olguín Dev <106123788+uwuolguin@users.noreply.github.com>
2024-04-09 13:08:53 -0400

    a

--- app/routers/firm.py
+++ app/routers/firm.py
@@ -13,20 +13,24 @@ from pydantic.functional_validators import BeforeValidator
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
-while True:
-    try:
-
-        conn_firm=psycopg2.connect(host=settings.database_hostname,database=settings.database_name,user=settings.database_username,password=settings.database_password,cursor_factory=RealDictCursor)
-        cursor=conn_firm.cursor()
-        break
-
-    except Exception as error:
-
-        print("Connecting to database failed")
-        print("Error:",error)
-        time.sleep(5)
 
 
+def getConnection():
+    
+
+    while True:
+        try:
+
+            conn_firm=psycopg2.connect(host=settings.database_hostname,database=settings.database_name,user=settings.database_username,password=settings.database_password,cursor_factory=RealDictCursor)
+            break
+
+        except Exception as error:
+
+            print("Connecting to database failed")
+            print("Error:",error)
+            time.sleep(5)
+
+    return conn_firm
 
 router= APIRouter(
     
@@ -38,40 +42,44 @@ router= APIRouter(
 def get_all_firm(login: str = Cookie(None)) -> Any:
 
     os.chdir(settings.normal_directory)
-    try:
-        conn_firm.rollback()
-    except:
-        pass
+
+    conn_firm=getConnection()
+    cursor=conn_firm.cursor()
 
     if login==None:
+        conn_firm.close()
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= "You need to be logged in to use this feature")
     
     credentials=oath2.decode_access_token(login)
 
     if dict(credentials).get("role") != "superadmin":
+        conn_firm.close()
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= "BAD CREDENTIALS")
     
     cursor.execute(""" SELECT * FROM """+settings.table_name_for_select_all_no_free_user+""" """)
     firm=cursor.fetchall()
     if not firm :
+        conn_firm.close()
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= "BBDD does not have any record")
+    conn_firm.close()
     return firm
 
 @router.get("/firm_get_id/id/{id}",response_model=schemas.firmResponse4)
 def get_firm(id:int,login: str = Cookie(None)) -> Any:
 
     os.chdir(settings.normal_directory)
-    try:
-        conn_firm.rollback()
-    except:
-        pass
+
+    conn_firm=getConnection()
+    cursor=conn_firm.cursor()
     
     if login==None:
+        conn_firm.close()
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= "You need to be logged in to use this feature")
     
     credentials=oath2.decode_access_token(login)
 
     if dict(credentials).get("role") != "superadmin":
+        conn_firm.close()
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= "BAD CREDENTIALS")
     
 
@@ -79,7 +87,9 @@ def get_firm(id:int,login: str = Cookie(None)) -> Any:
     cursor.execute(id_2 % (str(id)))
     firm=cursor.fetchone()
     if firm ==None:
+        conn_firm.close()
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"BBDD with id: {id} does not exist")
+    conn_firm.close()
     return firm
 
 
@@ -88,17 +98,18 @@ def get_firm(id:int,login: str = Cookie(None)) -> Any:
 def get_id_by_email_firm_2(email:EmailStr,login: str = Cookie(None)) -> Any:
 
     os.chdir(settings.normal_directory)
-    try:
-        conn_firm.rollback()
-    except:
-        pass
+
+    conn_firm=getConnection()
+    cursor=conn_firm.cursor()
    
     if login==None:
+        conn_firm.close()
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= "You need to be logged in to use this feature")
     
     credentials=oath2.decode_access_token(login)
 
     if dict(credentials).get("role") != "superadmin":
+        conn_firm.close()
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= "BAD CREDENTIALS")
     
   
@@ -106,7 +117,9 @@ def get_id_by_email_firm_2(email:EmailStr,login: str = Cookie(None)) -> Any:
     cursor.execute(id_2 % (str(email)))
     talent=cursor.fetchone()
     if talent ==None:
+        conn_firm.close()
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"BBDD with email: {str(email)} does not exist")
+    conn_firm.close()
     return talent
 
 
@@ -130,10 +143,9 @@ def post_firm(
                  )-> Any:
 
     os.chdir(settings.normal_directory)
-    try:
-        conn_firm.rollback()
-    except:
-        pass
+
+    conn_firm=getConnection()
+    cursor=conn_firm.cursor()
 
     hashed_password=utils.hash(password)
     password=hashed_password
@@ -145,9 +157,11 @@ def post_firm(
     contact_email=contact_email.replace(" ", "").lower()
 
     if email==settings.superadmin_email:
+        conn_firm.close()
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
     
     if contact_email==settings.superadmin_email:
+        conn_firm.close()
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
     
     cursor.execute(""" SELECT * FROM number_of_user_today();""")
@@ -155,6 +169,7 @@ def post_firm(
     for x in users_today.values():
         user_today_value=x 
     if user_today_value >=50:
+        conn_firm.close()
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail= "We are only receiving 50 login requests per day, sorry :( ")
 
 #################Validate Email is not a company###############
@@ -162,12 +177,14 @@ def post_firm(
     cursor.execute(validate_email % ((email)))
     validate_0=cursor.fetchone()
     if validate_0 :
+        conn_firm.close()
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail= "YOUR EMAIL IS ALREDY BEING USED")
 #################Validate Email is not a company###############
     validate_email=""" SELECT * FROM remove_firm_by_being_in_talent('%s');"""
     cursor.execute(validate_email % ((contact_email)))
     validate_0=cursor.fetchone()
     if validate_0 :
+        conn_firm.close()
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail= "YOUR EMAIL IS ALREDY BEING USED")
 
 
@@ -177,12 +194,14 @@ def post_firm(
     cursor.execute(validate_email % ((email)))
     validate_0=cursor.fetchone()
     if validate_0 :
+        conn_firm.close()
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail= "YOUR EMAIL IS BANNED")
     
     validate_email_2=""" SELECT * FROM remove_user_by_black_list_email('%s');"""
     cursor.execute(validate_email_2 % ((contact_email)))
     validate_0=cursor.fetchone()
     if validate_0 :
+        conn_firm.close()
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail= "YOUR EMAIL IS BANNED")
 
 #################Validate Blacklist Words ###############
@@ -191,17 +210,21 @@ def post_firm(
     cursor.execute(validate_word % ((full_name.replace(" ", "").lower()),(email_template_to_send.replace(" ", "").lower())))
     validate_1=cursor.fetchone()
     if validate_1 :
+        conn_firm.close()
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail= "THIS SITE DOES NOT ACCEPT BAD WORDS")
     
 #################Validate url contains words(linkedin,girhub,facebook,instagram) ###############
     
-    if not (str(linkedin).startswith('https://linkedin.com') ):   
+    if not (str(linkedin).startswith('https://linkedin.com') ):
+        conn_firm.close()   
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail= "the string should start with https://linkedin.com")
     
     if not (str(instagram).startswith('https://instagram.com')):
+        conn_firm.close()
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail= "the string should start with https://instagram.com")
     #################Validate url contains words(linkedin,girhub,facebook,instagram) ###############
-    if len (str(contact_phone) )>80:   
+    if len (str(contact_phone) )>80:
+        conn_firm.close()   
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail= "The number is too long")
     
 ###################CREATE USER#####################################################################
@@ -210,23 +233,24 @@ def post_firm(
     cursor.execute(firm_2 % (email,password,full_name,contact_email,str(contact_phone),email_template_to_send,linkedin,instagram))
 
     conn_firm.commit()
-
+    conn_firm.close()
 
 @router.delete("/firm_delete/id/",status_code=status.HTTP_204_NO_CONTENT)
 def delete_firm(login: str = Cookie(None)):
     
     os.chdir(settings.normal_directory)
-    try:
-        conn_firm.rollback()
-    except:
-        pass
+
+    conn_firm=getConnection()
+    cursor=conn_firm.cursor()
 
     if login==None:
+        conn_firm.close()
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= "You need to be logged in to use this feature")
     
     credentials=oath2.decode_access_token(login)
 
     if dict(credentials).get("role") != "firm":
+        conn_firm.close()
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= "BAD CREDENTIALS")
     
     id=dict(credentials).get("firm_id")
@@ -235,12 +259,13 @@ def delete_firm(login: str = Cookie(None)):
     cursor.execute(id_2 % (str(id)))
     firm=cursor.fetchone()
     if firm ==None:
+        conn_firm.close()
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"BBDD with id: {id} does not exist")
 
     delete=""" SELECT delete_firm_by_id('%s');"""
     cursor.execute(delete % (id))
     conn_firm.commit()
-
+    conn_firm.close()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
@@ -257,17 +282,18 @@ def update_firm(
                  )-> Any:
 
     os.chdir(settings.normal_directory)
-    try:
-        conn_firm.rollback()
-    except:
-        pass
+
+    conn_firm=getConnection()
+    cursor=conn_firm.cursor()
 
     if login==None:
+        conn_firm.close()
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= "You need to be logged in to use this feature")
     
     credentials=oath2.decode_access_token(login)
 
     if dict(credentials).get("role") != "firm":
+        conn_firm.close()
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= "BAD CREDENTIALS")
     
     id=dict(credentials).get("firm_id")
@@ -287,6 +313,7 @@ def update_firm(
     for x in users_today.values():
         user_today_value=x 
     if user_today_value >=50:
+        conn_firm.close()
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail= "We are only receiving 50 login requests per day, sorry :( ")
 
 #################Validate Email is not a company###############
@@ -294,6 +321,7 @@ def update_firm(
     cursor.execute(validate_email % ((contact_email)))
     validate_0=cursor.fetchone()
     if validate_0 :
+        conn_firm.close()
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail= "YOUR EMAIL IS ALREDY BEING USED")
 #################Validate Blacklist Email ###############
     
@@ -301,6 +329,7 @@ def update_firm(
     cursor.execute(validate_email_2 % ((contact_email)))
     validate_0=cursor.fetchone()
     if validate_0 :
+        conn_firm.close()
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail= "YOUR EMAIL IS BANNED")
 
 #################Validate Blacklist Words ###############
@@ -309,17 +338,21 @@ def update_firm(
     cursor.execute(validate_word % ((full_name.replace(" ", "").lower()),(email_template_to_send.replace(" ", "").lower())))
     validate_1=cursor.fetchone()
     if validate_1 :
+        conn_firm.close()
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail= "THIS SITE DOES NOT ACCEPT BAD WORDS")
     
 #################Validate url contains words(linkedin,girhub,facebook,instagram) ###############
     
     if not (str(linkedin).startswith('https://linkedin.com') ):   
+        conn_firm.close()
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail= "the string should start with https://linkedin.com")
     
     if not (str(instagram).startswith('https://instagram.com')):
+        conn_firm.close()
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail= "the string should start with https://instagram.com")
     #################Validate url contains words(linkedin,girhub,facebook,instagram) ###############
     if len (str(contact_phone) )>80:   
+        conn_firm.close()
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail= "The number is too long")
     
 
@@ -327,6 +360,7 @@ def update_firm(
     cursor.execute(id_2 % (str(id)))
     firm=cursor.fetchone()
     if firm ==None:
+        conn_firm.close()
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"BBDD with id: {id} does not exist")
 
     
@@ -335,7 +369,7 @@ def update_firm(
     conn_firm.commit()
 
 
-    
+    conn_firm.close()
     return Response(status_code=status.HTTP_201_CREATED,content='Data Updated') 
 
 
@@ -347,21 +381,12 @@ templates= Jinja2Templates(directory="./templates")
 @router.get('/signUpFirm/',response_class=HTMLResponse)
 def index(request: Request):
 
-    try:
-        conn_firm.rollback()
-    except:
-        pass
 
     context={'request': request}
     return templates.TemplateResponse("6_sign_up_firm.html",context)
 
 @router.get('/delUpFirm/',response_class=HTMLResponse)
 def delup(request: Request,login: str = Cookie(None)):
-
-    try:
-        conn_firm.rollback()
-    except:
-        pass
 
 
     if login==None:
