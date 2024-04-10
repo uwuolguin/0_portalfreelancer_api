0b71d7bf7f88cce17cee9e27e446a248440b4504
Andrés Olguín Dev <106123788+uwuolguin@users.noreply.github.com>
2024-04-10 12:37:49 -0400

    a

--- templates/0.DEV_scripts_main.js
+++ templates/0.DEV_scripts_main.js
@@ -22,21 +22,26 @@ function lensButtonRequest(){
   return false;
   }
 
-  async function logOut(){
 
 
-    await fetch("https://apiportalfreelancer.lat/auth/logout", {
-      headers: {
-        Accept: "application/json"
-      },
-      method: "DELETE"
-    })
-    .then((response) => console.log(response))
-    .catch((error) => console.log(error));
+  async function logOut() {
 
-    location.reload()
-
+      await fetch("https://apiportalfreelancer.lat/auth/logout/", {
+        headers: {
+          Accept: "application/json"
+        },
+        method: "DELETE"
+      }).then((response) => console.log(response)).catch((error) => console.log(error));
+    
+     }
 
   
+
+  async function reloadPageAfterLogOuT(){
+
+
+    await logOut()
+
+    console.log("logged out")
 
   }
