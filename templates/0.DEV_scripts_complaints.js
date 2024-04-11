5196a02fbc9706b50e1edcb98e7b65c990f0b1f6
Andrés Olguín Dev <106123788+uwuolguin@users.noreply.github.com>
2024-04-11 00:14:19 -0400

    a

--- templates/0.DEV_scripts_complaints.js
+++ templates/0.DEV_scripts_complaints.js
@@ -8,14 +8,14 @@ function post_succesful_visible_2(response){
     document.getElementById("succesful_post").classList.remove("hidden_div")
     document.getElementById("succesful_post").classList.remove("red_text")
     document.getElementById("succesful_post").classList.add("green_text")
-    document.getElementById("succesful_post").innerHTML="Succesful Login"
+    document.getElementById("succesful_post").innerHTML="Succesful Request"
 
   } else {
     
     document.getElementById("succesful_post").classList.remove("hidden_div")
     document.getElementById("succesful_post").classList.remove("green_text")
     document.getElementById("succesful_post").classList.add("red_text")
-    document.getElementById("succesful_post").innerHTML="Failed Login"
+    document.getElementById("succesful_post").innerHTML="Failed Request"
 
   }
 
