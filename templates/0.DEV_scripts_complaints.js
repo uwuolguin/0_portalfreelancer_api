5f1fe69ca99274a75580529171885364861cbc9e
Andrés Olguín Dev <106123788+uwuolguin@users.noreply.github.com>
2024-04-11 00:12:11 -0400

    a

--- templates/0.DEV_scripts_complaints.js
+++ templates/0.DEV_scripts_complaints.js
@@ -68,3 +68,49 @@ async function postLogIn() {
 
 
 } 
+function lensButtonRequest(){
+
+
+
+  
+  let glassValueStateButton= document.getElementById('lens_button').value
+  
+  if(glassValueStateButton==""){
+    
+    glassValueStateButton="None"
+  
+  }
+
+  
+  urlFinal=  "https://apiportalfreelancer.lat/contacts/contacts_normal/?skills_string=None&skills_state_string=None&category_string=None&category_state_string=None&pagination_state=1.2.3.4.5.6.7.8.9.10&pagination_value=1&magic_word="+glassValueStateButton
+  
+  console.log(urlFinal)
+  
+  
+  window.location.href =urlFinal
+  return false;
+  }
+
+
+
+  async function logOut() {
+
+      await fetch("https://apiportalfreelancer.lat/auth/logout", {
+        headers: {
+          Accept: "application/json"
+        },
+        method: "DELETE"
+      }).then((response) => console.log(response)).catch((error) => console.log(error));
+    
+     }
+
+  
+
+  async function reloadPageAfterLogOuT(){
+
+
+    await logOut()
+
+    location.reload()
+
+  }
