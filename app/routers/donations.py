5dca02a6f3a1686749cb43cc9cf6c1673687d04d
Andrés Olguín Dev <106123788+uwuolguin@users.noreply.github.com>
2024-04-14 22:17:59 -0400

    a

--- app/routers/donations.py
+++ app/routers/donations.py
@@ -19,9 +19,7 @@ router= APIRouter(
     tags=["Donations"]
 )
 
-@router.get("/")
-async def root():
-    return {"message": "Hello World"}
+
 
 #<script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="uwuolguin" data-description="Support me on Buy me a coffee!" data-message="Thanks for visiting. I hope you found the App and Documentation Useful" data-color="#BD5FFF" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
 
@@ -51,7 +49,7 @@ def complaints_html(request: Request,login: str = Cookie(None)):
 
 
             context={'request': request,'login_role':login_role_value}
-            return templates.TemplateResponse("11_complaints.html",context)
+            return templates.TemplateResponse("12_donations.html",context)
         except:
             time.sleep(1)
             pass
