11557a8547c441fcac046fca7b835d5eedf41a2d
Andrés Olguín Dev <106123788+uwuolguin@users.noreply.github.com>
2024-04-14 22:32:04 -0400

    a

--- app/routers/donations.py
+++ app/routers/donations.py
@@ -21,7 +21,7 @@ router= APIRouter(
 
 
 
-#<script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="uwuolguin" data-description="Support me on Buy me a coffee!" data-message="Thanks for visiting. I hope you found the App and Documentation Useful" data-color="#BD5FFF" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
+
 
 ################################################# TEMPLATES ####################################################################
     
