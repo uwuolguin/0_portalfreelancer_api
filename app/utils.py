e42a727818c69f3e71627178a53b4f314d5a39bd
Andrés Olguín Dev <106123788+uwuolguin@users.noreply.github.com>
2024-04-29 14:19:02 -0400

    a

--- app/utils.py
+++ app/utils.py
@@ -192,8 +192,8 @@ def validate_Image(id_var,file_var,endpoint):
 
 
 def testTableau():
-        tableau_auth = TSC.TableauAuth('USERNAME', 'PASSWORD', 'SITENAME')
-        server = TSC.Server('http://SERVER_URL')
+        tableau_auth = TSC.TableauAuth(settings.tableau_token_name, settings.tableau_token_password, settings.tableau_token_sitename)
+        server = TSC.Server(settings.tableau_token_server)
 
         with server.auth.sign_in(tableau_auth):
                 all_datasources, pagination_item = server.datasources.get()
