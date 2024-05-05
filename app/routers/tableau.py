8ec936929271799981b6a78fda138a631c1942af
Andrés Olguín Dev <106123788+uwuolguin@users.noreply.github.com>
2024-05-04 21:54:49 -0400

    a

--- app/routers/tableau.py
+++ app/routers/tableau.py
@@ -1,11 +1,32 @@
 from fastapi import  APIRouter,Cookie,Request,status,HTTPException
 from .. import oath2
+from ..config import settings
 import time
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+import psycopg2
+from psycopg2.extras import RealDictCursor
 from ..utils import tableauAuthentification,tableauAllDatasources,send_email_to_admin,tableauCreateWebhook,tableauListWebhook,tableauDeleteWebhook,tableauTestWebhook
 
 templates= Jinja2Templates(directory="./templates")
+
+def getConnection():
+    
+
+    while True:
+        try:
+
+            conn_tableau=psycopg2.connect(host=settings.database_hostname,database=settings.database_name,user=settings.database_username,password=settings.database_password,cursor_factory=RealDictCursor)
+            break
+
+        except Exception as error:
+
+            print("Connecting to database failed")
+            print("Error:",error)
+            time.sleep(5)
+
+    return conn_tableau
+
 
 router= APIRouter(
     
@@ -89,8 +110,23 @@ def tableau_test_webhook(webhookId:str,login: str = Cookie(None)):
 @router.post('/tableau_webhook_fail_refresh_destination/',status_code=status.HTTP_201_CREATED)
 def tableau_create_webhook(resource_name="Test"):
 
-    send_email_to_admin('refresh of extraction failed'+' datasourceName='+resource_name)
+    conn_tableau=getConnection()
+    cursor=conn_tableau.cursor()
+    
+    cursor.execute(""" select count(tableau_url) from public.tableau_failed_refreshed where last_updated_at + interval '1 day' >=NOW() :: TIMESTAMP WITH TIME ZONE and tableau_url = "failed_refreshed_webhook"; """)
+    
+    counter=complaint=cursor.fetchone()
+    
+    if counter<3:
 
+        send_email_to_admin('refresh of extraction failed'+' datasourceName='+resource_name)
+        
+        cursor.execute(""" INSERT INTO public.tableau_failed_refreshed (tableau_url) VALUES (failed_refreshed_webhook); """)
+
+        conn_tableau.commit()
+
+    
+    conn_tableau.close()
     return status.HTTP_201_CREATED
 
 
