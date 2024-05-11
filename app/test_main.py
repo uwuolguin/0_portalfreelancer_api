bc7f8c24a597fea6dc344b514c4f4cb96b0821b7
Andrés Olguín Dev <106123788+uwuolguin@users.noreply.github.com>
2024-05-11 00:27:13 -0400

    a

--- app/test_main.py
+++ app/test_main.py
@@ -76,4 +76,23 @@ def test_log_out(email):
 
     )
     assert response.status_code == 200
-    # assert response.json() == expected
+    assert response.json() == {
+                                "status": "success"
+                              }
+
+####################################### Testing the endpoints in the blacklistemail.py file
+
+def test_get_all_black_list_email():
+
+    login_cookie=create_cookie_token_access_for_testing(email=settings.superadmin_email)
+
+    client.cookies={"login": login_cookie}
+
+    response = client.get(
+        "/blacklistemail/",
+        
+        headers= {"Accept": "application/json",
+                 },
+
+    )
+    assert response.status_code == 200
