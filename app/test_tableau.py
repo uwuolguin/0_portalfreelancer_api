fe8ebf703daf953633e33df746cb3a9e119869d7
Andrés Olguín Dev <106123788+uwuolguin@users.noreply.github.com>
2024-05-13 19:26:12 -0400

    a

--- app/test_tableau.py
+++ app/test_tableau.py
@@ -50,55 +50,45 @@ def test_get_webhook_tableau_html():
     )
     assert response.status_code == 200
 
-# def test_post_categories():
 
-#     login_cookie=create_cookie_token_access_for_testing(email=settings.superadmin_email)
+def test_post_test_delete_webhook_tableau():
 
-#     client.cookies={"login": login_cookie}
+    login_cookie=create_cookie_token_access_for_testing(email=settings.superadmin_email)
 
-#     response = client.post(
-#         "/categories/category_post/",
+    client.cookies={"login": login_cookie}
+
+    response = client.post(
+        url="https://apiportalfreelancer.lat/tableau/tableau_create_webhook/?webhookname=delete_test30&webhookUrl=https://apiportalfreelancer.lat/tableau/tableau_webhook_fail_refresh_destination/&event=ViewDeleted",
         
-#         headers= {"Accept": "application/json",
-#                   "Content-Type": "application/x-www-form-urlencoded"
-#                  },
-#         data= {'category_input': 'hola',
+        headers= {"Accept": "application/json",
+
+                 },
+
+    )
+    id_=response.json().split(",")[5].split(":")[1].replace('''"''',"")
 
 
-#         }
-
-#     )
-#     assert response.status_code == 201
-
-    
-# def test_update_category():
-
-#     login_cookie=create_cookie_token_access_for_testing(email=settings.superadmin_email)
-
-#     client.cookies={"login": login_cookie}
-
-#     url_input="/categories/category_put/hola/hola2"
-
-#     response = client.put(
-#         url=url_input,
-
-#     )
-#     assert response.status_code == 201
-
-
-# def test_delete_category():
-
-#     login_cookie=create_cookie_token_access_for_testing(email=settings.superadmin_email)
-
-#     client.cookies={"login": login_cookie}
-
-#     url_input="/categories/category_delete/id/hola2"
-
-#     response = client.delete(
-#         url=url_input,
+    response_test = client.get(
+        url="https://apiportalfreelancer.lat/tableau/tableau_test_webhook/?webhookId="+id_,
         
-#         headers= {"Accept": "*/*",
-#                  },
-#     )
-#     assert response.status_code == 204
-####################################### Testing the normal endpoints in the tableau.py file
+        headers= {"Accept": "application/json",
+
+                 },
+
+    )
+
+    url_delete="https://apiportalfreelancer.lat/tableau/tableau_delete_webhook/?webhookId="+id_
+
+    response_delete = client.delete(
+        url=url_delete,
+        
+        headers= {"Accept": "*/*",
+                 },
+    )
+
+
+    assert response.status_code == 201
+    assert response_test.status_code == 200
+    assert response_delete.status_code == 204
+
+
