8e32e073d32329f1036be264296a7d4be8d6dcab
Andrés Olguín Dev <106123788+uwuolguin@users.noreply.github.com>
2024-05-14 19:20:56 -0400

    a

--- app/test_contacts.py
+++ app/test_contacts.py
@@ -41,4 +41,19 @@ def test_get_all_contacts():
     )
     assert response.status_code == 200
 
+def test_get_contacts_talent_id():
+
+    login_cookie=create_cookie_token_access_for_testing(email=settings.superadmin_email)
+
+    client.cookies={"login": login_cookie}
+
+    response = client.get(
+        url="https://apiportalfreelancer.lat/contacts/contact_get_id/id_talent/39",
+        
+        headers= {"Accept": "application/json",
+                 },
+
+    )
+    assert response.status_code == 200
+
 
