49db8b9cd33d8226c23078c30bca6ae48256fa3e
Andrés Olguín Dev <106123788+uwuolguin@users.noreply.github.com>
2024-05-18 22:32:12 -0400

    a

--- app/test_talent.py
+++ app/test_talent.py
@@ -156,6 +156,8 @@ def test_post_update_delete_talent():
 
     os.chdir(settings.normal_directory)
 
+    ####PUT
+
     login_cookie=create_cookie_token_access_for_testing(email="delete_for_testing_talent@gmail.com")
 
     client.cookies={"login": login_cookie}
@@ -182,97 +184,80 @@ def test_post_update_delete_talent():
 
     )
 
+
+    ######DELETE
+    response_delete = client.delete(
+        url="https://apiportalfreelancer.lat/talent/talent_delete/id/",
+        
+        headers= {"Accept": "*/*",
+                 },
+    )
+
     assert response_post.status_code == 201
     assert response_put.status_code == 201
-    
-
-#     ####PUT
-    
-#     login_cookie=create_cookie_token_access_for_testing(email="delete_for_testing@gmail.com")
-
-#     print("login cookie: "+login_cookie)
-
-#     client.cookies={"login": login_cookie}
-
-#     url_input="https://apiportalfreelancer.lat/firm/firm_put/"
-
-#     response_put = client.put(
-         
-#         url=url_input,
-
-#         headers= {"Accept": "application/json",
-#                   "Content-Type": "application/x-www-form-urlencoded"
-#                  },
-#         data= {'password':'test',
-#                'full_name':'test_has_changed',
-#                'contact_email':'delete_for_testing@gmail.com',
-#                'contact_phone':123456789,
-#                'email_template_to_send':'test',
-#                'linkedin':'https://linkedin.com/',
-#                'instagram':'https://instagram.com/',
+    assert response_delete.status_code == 204
 
 
-#         }
+def test_delete_talent_by_admin():
 
-#     )
-#     assert response_put.status_code == 201
+    ###POST
 
-#     ######DELETE
-#     response_delete = client.delete(
-#         url="https://apiportalfreelancer.lat/firm/firm_delete/id/",
+    os.chdir(settings.picture_directory)
+
+    picture=open('example2.png', 'rb')
+
+    response_post = client.post(
+        "/talent/talent_post/",
         
-#         headers= {"Accept": "*/*",
-#                  },
-#     )
-#     assert response_delete.status_code == 204
 
+        data= {
+            'email': 'delete_for_testing_talent@gmail.com',
+            'password':'test',
+            'full_name':'test',
+            'profession':'test',
+            'rate':123,
+            'description':'test',
+            'linkedin':'https://linkedin.com/',
+            'github':'https://github.com/',
+            'instagram':'https://instagram.com/',
+            'facebook':'https://facebook.com/',
+            'skills':'test',
+            'categories':'test',
 
-# def test_delete_firm_by_admin():
+        },
 
-#     ###POST
+        files={'file': picture}
+
+    )
     
-#     response_post = client.post(
-#         "https://apiportalfreelancer.lat/firm/firm_post/",
+    picture.close()
+
+    os.chdir(settings.normal_directory)
+
+    assert response_post.status_code == 201
+
+    try:
+
+            conn_test=getConnection()
+            cursor=conn_test.cursor()
+
+            SQL_STATEMENT_TALENT_ID="SELECT id FROM public.talent where email ='delete_for_testing_talent@gmail.com';" 
+
+            cursor.execute(SQL_STATEMENT_TALENT_ID)
+
+            id_talent=cursor.fetchone().get("id")
+
+            conn_test.close()
+    except:
+            pass
+    login_cookie=create_cookie_token_access_for_testing(email=settings.superadmin_email)
+
+    client.cookies={"login": login_cookie}
+
+    response_delete = client.delete(
+        "/talent/talent_delete_by_admin/id/"+str(id_talent),
         
-#         headers= {"Accept": "application/json",
-#                   "Content-Type": "application/x-www-form-urlencoded"
-#                  },
-#         data= {'email': 'delete_for_testing_admin@gmail.com',
-#                'password':'test',
-#                'full_name':'test',
-#                'contact_email':'delete_for_testing@gmail.com',
-#                'contact_phone':123456789,
-#                'email_template_to_send':'test',
-#                'linkedin':'https://linkedin.com/',
-#                'instagram':'https://instagram.com/',
-
-#         }
-
-#     )
-#     assert response_post.status_code == 201
-
-#     try:
-
-#             conn_test=getConnection()
-#             cursor=conn_test.cursor()
-
-#             SQL_STATEMENT_FIRM_ID="SELECT id FROM public.firm where email ='delete_for_testing_admin@gmail.com';" 
-
-#             cursor.execute(SQL_STATEMENT_FIRM_ID)
-
-#             id_firm=cursor.fetchone().get("id")
-
-#             conn_test.close()
-#     except:
-#             pass
-#     login_cookie=create_cookie_token_access_for_testing(email=settings.superadmin_email)
-
-#     client.cookies={"login": login_cookie}
-
-#     response_delete = client.delete(
-#         url="https://apiportalfreelancer.lat/firm/firm_delete_by_admin/id/"+str(id_firm),
-        
-#         headers= {"Accept": "*/*",
-#                  },
-#     )
-#     assert response_delete.status_code == 204
+        headers= {"Accept": "*/*",
+                 },
+    )
+    assert response_delete.status_code == 204
