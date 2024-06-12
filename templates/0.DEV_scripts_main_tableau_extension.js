b62f40254ea4a7df97ecb5442d8e0fe363952e8d
Andrés Olguín Dev <106123788+uwuolguin@users.noreply.github.com>
2024-06-12 18:35:58 -0400

    a

--- templates/0.DEV_scripts_main_tableau_extension.js
+++ templates/0.DEV_scripts_main_tableau_extension.js
@@ -18,7 +18,7 @@
     const dataTable = await dataTableReader.getAllPagesAsync();
     await dataTableReader.releaseAsync();
 
-    console.log(dataTable[0][0])
+    console.log(dataTable.columns)
 
   }
 //
